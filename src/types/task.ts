e5640a5c51ecbcdee9451d0388502d0e3852e605
dakravchenko <daniil.kravchenko@gmail.com>
2025-11-15 08:19:46 +0100

    updates types

--- src/types/task.ts
+++ src/types/task.ts
@@ -1,8 +1,10 @@
 import { Task } from "@prisma/client";
 
-export type OptionalTask = Omit<Task, 'id' | 'createdAt' | 'updatedAt' | 'number'> & {
-    id?: string;
-    createdAt?: Date;
-    updatedAt?: Date;
-    number?: number;
-  };
+// export type OptionalTask = Omit<Task, 'id' | 'createdAt' | 'updatedAt' | 'number'> & {
+//     id?: string;
+//     createdAt?: Date;
+//     updatedAt?: Date;
+//     number?: number;
+//   };
+
+export type OptionalTask = Partial<Task>;
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,10 +1,12 @@
-import { Project, ResourceType, Task } from "@prisma/client";
+import { Project, ResourceType, Task, User } from "@prisma/client";
 
-export type ReducedUser = {
-  id: string;
-  name: string;
-  email: string;
-};
+// export type ReducedUser = {
+//   id: string;
+//   name: string;
+//   email: string;
+// };
+
+export type ReducedUser = Pick<User, "id" | "name" | "email">;
 
 export type FullProject = Project & {
   members: ReducedUser[];
