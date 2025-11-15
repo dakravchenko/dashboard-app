2e3a9bc57fb21ad42782240bd9e4a44a2669579b
dakravchenko <daniil.kravchenko@gmail.com>
2025-11-15 08:28:31 +0100

    uses omit for task and readd propertiesas optional

--- src/types/task.ts
+++ src/types/task.ts
@@ -1,10 +1,11 @@
 import { Task } from "@prisma/client";
 
-// export type OptionalTask = Omit<Task, 'id' | 'createdAt' | 'updatedAt' | 'number'> & {
-//     id?: string;
-//     createdAt?: Date;
-//     updatedAt?: Date;
-//     number?: number;
-//   };
-
-export type OptionalTask = Partial<Omit<Task, 'level'>> & { level: Task['level'] };
+export type OptionalTask = Omit<
+  Task,
+  "id" | "createdAt" | "updatedAt" | "number"
+> & {
+  id?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  number?: number;
+};
